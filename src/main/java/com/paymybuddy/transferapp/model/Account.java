cc495e862a03b6b647af5c55aad3fabb06114779
Frederic Picard <fred.picard@gmail.com>
2023-06-08 13:01:51 +0200

    User and Account entities with relationships

---
+++ src/main/java/com/paymybuddy/transferapp/model/Account.java
@@ -0,0 +1,26 @@
+package com.paymybuddy.transferapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "account")
+@Data
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+
+    private Integer number;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    public Account() {
+
+    }
+}
+
---
+++ src/main/java/com/paymybuddy/transferapp/model/User.java
@@ -0,0 +1,33 @@
+package com.paymybuddy.transferapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String firstName;
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    @OneToMany(
+            mappedBy = "user"
+    )
+    private List<Account> accounts = new ArrayList<>();
+
+    public User() {
+
+    }
+}
+
